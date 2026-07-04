368d1c078b14c46770056812d3db9d172dd62a46
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-03 22:22:18 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,42 @@
+export default function Home() {
 
+  return (
+
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#F7F5EF"
+      }}
+    >
+
+      <div>
+
+        <h1
+          style={{
+            fontSize: "60px",
+            color: "#184C3A",
+            marginBottom: "15px"
+          }}
+        >
+          🌿 Naturland Hub
+        </h1>
+
+        <p
+          style={{
+            fontSize: "20px",
+            color: "#4B5E53"
+          }}
+        >
+          Sistema Integral de Autenticación
+        </p>
+
+      </div>
+
+    </main>
+
+  );
+
+}

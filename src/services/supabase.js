887df6ff539c61ec5f23625f33d89d1651a26f2e
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-04 08:08:39 -0500

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bodudwldyumddshyamsj.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-const supabaseKey =
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvZHVkd2xkeXVtZGRzaHlhbXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMTM2MTMsImV4cCI6MjA5ODY4OTYxM30.G9Cer6HWevpAKj_pCrhTQitsqaUfUKDY1n4Vu2ehZ2Q;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
-  supabaseKey
+  supabaseAnonKey
 );

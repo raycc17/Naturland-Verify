9ffffd27b26702b63b6fb51660b1273ee3b9169a
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-04 08:20:49 -0500

    Create theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+import colors from "./colors";
+import spacing from "./spacing";
+import shadows from "./shadows";
+import typography from "./typography";
+
+const theme = {
+
+  colors,
+
+  spacing,
+
+  shadows,
+
+  typography,
+
+  radius: "18px",
+
+  maxWidth: "1400px"
+
+};
+
+export default theme;

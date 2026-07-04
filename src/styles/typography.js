f04468d20f8753464d0e99ea2edd9d9f92b5edbb
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-04 08:20:34 -0500

    Create typography.js

---
+++ src/styles/typography.js
@@ -0,0 +1,25 @@
+const typography = {
+
+  fontFamily: "Gordita, sans-serif",
+
+  thin: 300,
+
+  regular: 400,
+
+  medium: 500,
+
+  bold: 600,
+
+  h1: "42px",
+
+  h2: "30px",
+
+  h3: "22px",
+
+  body: "16px",
+
+  small: "14px"
+
+};
+
+export default typography;

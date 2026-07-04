8eff0d8a3fa90c0e3d10e80d4c798ce17ffde9cb
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-04 08:18:52 -0500

    Create colors.js

---
+++ src/styles/colors.js
@@ -0,0 +1,23 @@
+const colors = {
+
+  primary: "#052b04",
+
+  cream: "#ece6d3",
+
+  white: "#FFFFFF",
+
+  gold: "#B88430",
+
+  danger: "#a20d0d",
+
+  info: "#5979bd",
+
+  text: "#1E1E1E",
+
+  textLight: "#777777",
+
+  border: "#DDD9CC"
+
+};
+
+export default colors;

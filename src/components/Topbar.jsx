f6b84fb56aa16a8afc5f62488165258e3dba8dc8
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-04 07:49:13 -0500

    Create Topbar.jsx

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,36 @@
+export default function Topbar() {
+
+  return (
+
+    <div
+      style={{
+        background: "#FFFFFF",
+        padding: "25px 35px",
+        borderRadius: "16px",
+        boxShadow: "0 8px 20px rgba(0,0,0,.05)",
+        marginBottom: "35px"
+      }}
+    >
+
+      <h2
+        style={{
+          color:"#184C3A"
+        }}
+      >
+        Bienvenida, Raysa 👋
+      </h2>
+
+      <p
+        style={{
+          color:"#777",
+          marginTop:"8px"
+        }}
+      >
+        Centro de Operaciones Naturland
+      </p>
+
+    </div>
+
+  );
+
+}

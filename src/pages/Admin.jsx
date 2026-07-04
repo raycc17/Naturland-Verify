64c1f7a7cc97bfbc06ac9539c463cdf43a246227
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-03 22:22:46 -0500

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,1 +1,27 @@
+export default function Admin() {
 
+  return (
+
+    <main
+      style={{
+        padding: "60px"
+      }}
+    >
+
+      <h1>
+
+        Centro de Operaciones
+
+      </h1>
+
+      <p>
+
+        Bienvenida al panel de Naturland Hub.
+
+      </p>
+
+    </main>
+
+  );
+
+}

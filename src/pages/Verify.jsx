7c225aad87d79a0a9d3a1e8ad6cd0812f3611dec
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-03 22:22:31 -0500

    Update Verify.jsx

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,1 +1,27 @@
+export default function Verify() {
 
+  return (
+
+    <main
+      style={{
+        padding: "60px"
+      }}
+    >
+
+      <h1>
+
+        Verificar Producto
+
+      </h1>
+
+      <p>
+
+        Aquí el cliente ingresará su código único.
+
+      </p>
+
+    </main>
+
+  );
+
+}

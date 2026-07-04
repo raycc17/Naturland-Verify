cba30537fc5a3851c3235d08a984341b460b1118
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-04 08:06:40 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Verify from "./pages/Verify";
 import Admin from "./pages/Admin";
+import Products from "./pages/Products";
 
 export default function App() {
 
@@ -15,6 +16,8 @@ export default function App() {
       <Route path="/verify" element={<Verify />} />
 
       <Route path="/admin" element={<Admin />} />
+
+      <Route path="/products" element={<Products />} />
 
     </Routes>
 

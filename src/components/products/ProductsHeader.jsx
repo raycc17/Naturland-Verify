840e5bc80d9898b26f6ddcff79a29a34da7e5d8f
raycc17 <raycoroneldelcarpio@gmail.com>
2026-07-11 15:36:24 -0500

    Update ProductsHeader.jsx

--- src/components/products/ProductsHeader.jsx
+++ src/components/products/ProductsHeader.jsx
@@ -1,41 +1,23 @@
-import { useState } from "react";
-
 import Button from "../ui/Button";
 import Title from "../ui/Title";
 
-import Modal from "../ui/Modal";
-import ProductForm from "./ProductForm";
-
-export default function ProductsHeader() {
-
-  const [open, setOpen] = useState(false);
-
+export default function ProductsHeader({ onNewProduct }) {
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "35px"
-        }}
-      >
-        <Title marginBottom="0">
-          Productos
-        </Title>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "35px",
+      }}
+    >
+      <Title marginBottom="0">
+        Productos
+      </Title>
 
-        <Button onClick={() => setOpen(true)}>
-          + Nuevo Producto
-        </Button>
-      </div>
-
-      <Modal
-        open={open}
-        title="Registrar nuevo producto"
-        onClose={() => setOpen(false)}
-      >
-        <ProductForm />
-      </Modal>
-    </>
+      <Button onClick={onNewProduct}>
+        + Nuevo Producto
+      </Button>
+    </div>
   );
 }
